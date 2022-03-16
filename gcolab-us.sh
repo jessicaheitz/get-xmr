cbd18dcfaec8c0203369848ff9a3a7b279e5d365
jessicaheitz <101132550+jessicaheitz@users.noreply.github.com>
2022-03-16 16:21:32 +0300

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
 tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig -o us-west.minexmr.com:443 -u 8A6cdvshzV4dEWthU6fdgjMoJaNgwTn7rLDSdpLoKtHrQ83cdQCWtQQe6HGDrnYv5Z3zUfGuKSR8AByBGpgVPkc8Kp95en3 -k --tls --rig-id GC-US
+./xmrig -o us-west.minexmr.com:443 -u 8AJMDgZdaqPHSNACwNjam3NXJcVvCWdYqd6qTShTkxcQbMprvv5Sf9jL37uXKhm6LxTS9g1d1MSheARkpGoMcGxN5ZFqFQU -k --tls --rig-id GC-US
